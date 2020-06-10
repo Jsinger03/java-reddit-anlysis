cb975f32ef47bb82e1e91ead59e38b2a262f2f80
KarlWithK <jocelinc60@outlook.com>
2020-06-10 15:44:22 -0400

    updated json

--- src/JsonParse.java
+++ src/JsonParse.java
@@ -1,23 +1,31 @@
 // package src;
 // use the gson jar with -cp "gson-2.8.6.jar:."
-import com.google.gson.*; 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JsonParse {
-	private String commentSource = "../Comments.json"
+	private String commentSource = "../Comments.json";
 	public static void main (String[] args) {
-		
+		parse();
 	}	
 
-	public static void parse() {
+	public ArrayList<RedditComment> parse() {
 		try {
+			int count = 0;
 			File redditComments = new File(commentSource);
+			ArrayList<RedditComment> comments = new ArrayList<RedditComment>();
 			Scanner reader = new Scanner(redditComments);
-
+			GsonBuilder builder = new GsonBuilder();
+			builder.setPrettyPrinting();
+			Gson gson = builder.create();
 			while (reader.hasNextLine()) {
-
-				
+				String data = reader.nextLine();
+				RedditComment comment = gson.fromJson(data, RedditComment.class);
+				comments.add(comment);
+				count++;
 			}
 
 
--- src/RedditComment.java
+++ src/RedditComment.java
@@ -2,17 +2,24 @@ public class RedditComment {
 	private String body;
 	private String author;
 	private String subreddit;
+	private String link_id;
+	private String parent_id;
+	private String adjective;	
 	private int score;
-	private int link_id;
-	private int parent_id;
 	private int created_utc;
 	private int bodyScore;
 	
 
-	// databaase (reddit commentSource <- JsonParse) -> RedditComment (ArrayList -> subreddit -> sort()) -> gui (inputs) -> RedditComment -> parse sum number (or take the bodyScore) -> 
+	// databaase (reddit commentSource <- JsonParse) -> RedditComment (ArrayList -> subreddit -> sort()) -> gui (inputs) -> RedditComment -> parse sum number (or take the bodyScore) -> gui display 10 commments 
+	//
+	// Limations: 1) slow 2) words
 	
 	RedditComment() {
 					
+	}
+
+	public void setBodyScore(int bodyScore) {
+		this.bodyScore = bodyScore;
 	}
 
 	/**
@@ -46,14 +53,14 @@ public class RedditComment {
 	/**
 	 * @return the link_id
 	 */
-	public int getLink_id() {
+	public String getLink_id() {
 		return link_id;
 	}
 
 	/**
 	 * @return the parent_id
 	 */
-	public int getParent_id() {
+	public String getParent_id() {
 		return parent_id;
 	}
 
@@ -73,5 +80,61 @@ public class RedditComment {
 		return "RedditComment [author:" + author + ", body:" + body + ", created_utc:" + created_utc + ", link_id:"
 				+ link_id + ", parent_id:" + parent_id + ", score:" + score + ", subreddit:" + subreddit + "]";
 }
+
+	/**
+	 * @param body the body to set
+	 */
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/**
+	 * @param subreddit the subreddit to set
+	 */
+	public void setSubreddit(String subreddit) {
+		this.subreddit = subreddit;
+	}
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	/**
+	 * @param link_id the link_id to set
+	 */
+	public void setLink_id(String link_id) {
+		this.link_id = link_id;
+	}
+
+	/**
+	 * @param parent_id the parent_id to set
+	 */
+	public void setParent_id(String parent_id) {
+		this.parent_id = parent_id;
+	}
+
+	/**
+	 * @param created_utc the created_utc to set
+	 */
+	public void setCreated_utc(int created_utc) {
+		this.created_utc = created_utc;
+	}
+
+	/**
+	 * @return the bodyScore
+	 */
+	public int getBodyScore() {
+		return bodyScore;
+	}
 
 }
